3b7fad37e01e5d4c1616babc662bd3d4b49e82a0
Алексей Сотников <alex2004sotnikov2@yandex.ru>
2024-12-20 00:04:16 +0400

    regChange

--- backendSrc/routes/userRoutes.js
+++ backendSrc/routes/userRoutes.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const authController = require('../controllers/authController'); // Добавляем контроллер для регистрации
 
 // Маршруты для пользователей
 router.get('/', userController.getAllUsers); // Получить всех пользователей
 router.get('/:id', userController.getUserById); // Получить пользователя по ID
 router.put('/:id', userController.updateUser); // Обновить данные пользователя
 router.delete('/:id', userController.deleteUser); // Удалить пользователя
+
+// Добавляем маршрут для регистрации
+router.post('/', authController.register);
 
 module.exports = router;
