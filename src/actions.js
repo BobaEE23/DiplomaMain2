2f7b7b5cc9d5f8d8f8ece346c38cb81e36b00fba
Алексей Сотников <alex2004sotnikov2@yandex.ru>
2024-12-19 20:57:57 +0400

    testEnd

--- src/actions.js
+++ src/actions.js
@@ -1,6 +1,4 @@
 
-
-
 export const setUserRole = (role) => ({
   type: 'SET_USER_ROLE',
   payload: role,
@@ -50,7 +48,7 @@ export const deleteProduct = (productId) => ({
 export const fetchProducts = () => {
   return async (dispatch) => {
     try {
-      const response = await fetch(process.env.REACT_APP_API_URL);
+      const response = await fetch('https://diploma-r63e.onrender.com/api/products');
       
       if (!response.ok) {
         throw new Error('Ошибка при получении данных');
